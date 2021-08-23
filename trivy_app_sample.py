db8b62c42531eeebc1b0dddfb6f6f3f684a0c762
MoOyeg <mojojoye@yahoo.com>
2021-08-23 11:17:17 -0400

    updates

--- trivy_app_sample.py
+++ trivy_app_sample.py
@@ -7,4 +7,4 @@ from app import create_app # pylint: disable=unused-import
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port='8080')  
+    app.run(debug=True,host='0.0.0.0',port='8080')
